627763e7d7dbe15a9ad470db7356e93e380fa8fd
robin025 <rsingh8@umassd.edu>
2025-03-20 18:55:33 -0400

    fixed images

--- app/components/MockupShowcase.tsx
+++ app/components/MockupShowcase.tsx
@@ -1,17 +1,17 @@
 // Mockup image data
 const mockups = [
   {
-    image: './mockups/Command_pallete.png',
+    image: '/mockups/Command_pallete.png',
     title: 'Command Palette',
     description: 'Access your prompts instantly with a powerful command palette interface',
   },
   {
-    image: './mockups/prompt_manager.png',
+    image: '/mockups/prompt_manager.png',
     title: 'Prompt Manager',
     description: 'Organize and manage all your prompts in one beautiful interface',
   },
   {
-    image: './mockups/popup.png',
+    image: '/mockups/popup.png',
     title: 'Quick Access',
     description: 'Spotlight-inspired design for accessing your prompts anywhere',
   },
